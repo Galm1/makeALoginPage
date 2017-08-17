10c0e4253197ba89592df62777af9bdeb7cb5c85
EliGarcia <garciaelisha93@yahoo.com>
2017-08-17 16:49:40 -0500

    close

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(expressValidator());
 app.use(session({
   secret: 'keyboard cat',
@@ -17,10 +19,16 @@ app.use(session({
   saveUninitialized: true
 }))
 
-app.use(function (req, res, next) {
+const info = []
 
-  next()
-})
+// app.use(function(req, res, next) {
+//   if (req.url == '/login') {
+//     next()
+//   } else if (!req.session.username) {
+//      res.render('login')
+//   } next()  
+// }
+// })
 
 app.get('/', function(req, res) {
   res.render('index')
@@ -29,6 +37,15 @@ app.get('/', function(req, res) {
 app.post('/login', function(req, res) {
   console.log('username is ' + req.body.username);
   console.log('password is ' + req.body.password);
+
+  // loop through array to find username and password
+  // if you find a match then set req.session.login =true
+  // req.session.user = req.body.username
+  // res.render('index')
+
+  // if not then render login with an error message
+  // res.render('login')
+
   res.render('index')
 })
 
