0663495f9b0a6652d1bb89c8a851af6549f9109c
EliGarcia <garciaelisha93@yahoo.com>
2017-08-17 15:40:59 -0500

    barebones

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('what up yo');
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const app = express();
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(expressValidator());
+
+app.get('/', function(req, res) {
+  res.send('I am ALIIIIVE')
+});
+
+app.listen(3000, function() {
+  console.log('Successfully started express application!');
+});
