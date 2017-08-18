0cd7ee4a95127eb412e277062599c832850ad814
EliGarcia <garciaelisha93@yahoo.com>
2017-08-17 19:04:00 -0500

    'tis fuctional

--- app.js
+++ app.js
@@ -1,54 +1,65 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
-const bodyParser = require('body-parser');
-const expressValidator = require('express-validator');
+const parseurl = require('parseurl');
 const session = require('express-session');
+const bodyParser = require('body-parser')
 const app = express();
-
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
-app.use(expressValidator());
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: true
 }))
 
-const info = []
+let profiles = [{
+  username: 'courtney',
+  password: 'isbadatcoding'
+}, {
+  username: 'sorry',
+  password: 'notsorry'
+}];
 
-// app.use(function(req, res, next) {
-//   if (req.url == '/login') {
-//     next()
-//   } else if (!req.session.username) {
-//      res.render('login')
-//   } next()  
-// }
-// })
+app.use(function(req, res, next) {
+
+  if (req.url === '/login') {
+    next();
+  } else if (!req.session.login) {
+    res.render('login');
+  } else {
+    next();
+  }
+
+})
 
 app.get('/', function(req, res) {
   res.render('index')
-});
-
-app.post('/login', function(req, res) {
-  console.log('username is ' + req.body.username);
-  console.log('password is ' + req.body.password);
-
-  // loop through array to find username and password
-  // if you find a match then set req.session.login =true
-  // req.session.user = req.body.username
-  // res.render('index')
-
-  // if not then render login with an error message
-  // res.render('login')
-
-  res.render('index')
 })
 
+app.post('/login', function(req, res) {
+  let username = req.body.username;
+  let password = req.body.password;
+  console.log('username input = ' + username);
+  console.log('password input = ' + password);
+  for (var i = 0; i < profiles.length; i++) {
+    if (username === profiles[i].username && password === profiles[i].password) {
+      req.session.login = true;
+    }
+  }
+  if (req.session.login === true) {
+    res.render('index');
+  } else {
+    res.render('login', {
+      error: "incorrect username or password"
+    });
+  }
+
+
+})
 app.listen(3000, function() {
-  console.log('Successfully started express application!');
-});
+  console.log('YOUR SERVER IS RUNNING ,BOSS')
+})
